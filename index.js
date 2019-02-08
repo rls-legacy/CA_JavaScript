9b45b39b06837d146f2b1642665a7f8236b73ab9
fatalcenturion <tom.b.2k2@gmail.com>
2019-02-08 18:23:38 +0000

    updated index.js
    adding functions to the command handler...

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ const fs = require('fs');
 const config = require('./data.js');
 client.login(config.token);
 client.on('ready', ()=>{
-  console.log('Capt\'n Amelia is now ready')
+  console.log(`Capt'n Amelia is now ready for ${client.guilds.size} servers`)
+})
+
+client.on('message', (message)=>{
+  if(message.author.bot === true){
+    return;
+  } else {
+    var prefix = config.prefix
+    var messageArray = message.content.split(' ')
+    if(messageArray.includes(prefix)){
+      var cmd = messageArray[0].slice(prefix.length)
+      var args = messageArray.slice(1)
+      console.log("Cmd : " + cmd + " Args : " + args)
+    } else {
+      return;
+    }
+  }
 })
